66bc87274fc716364c60d3685623c7136cfc2d8d
Amirhosein <amir.vejahat.av@gmail.com>
2019-07-27 07:24:34 +0430

    add some functionality

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import url
 from core import views
-
+from django.contrib import admin
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
-    url(r'^upload/', views.simple_upload, name="upload"),
-    url(r'^test/',views.test,name="test"),
+    url(r'^upload/',views.simple_upload, name="upload"),
     url(r'^course/(?P<topic_name>\w+)',views.course,name="course")
 
 
